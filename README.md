0600829c8229ccffd6f0242a4f447ef892e3ec49
benjamin <benschac@gmail.com>
2023-01-13 13:08:42 -0600

    install and use lib

--- README.md
+++ README.md
@@ -32,3 +32,4 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+<img width="1728" alt="image" src="https://user-images.githubusercontent.com/2502947/212391853-454df692-2f20-47e6-866f-9c8e9c12e2c9.png">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { u8aToHex } from "@polkadot/util";
+import { bip39Generate, bip39ToSeed, waitReady } from "@polkadot/wasm-crypto";
+import React from "react";
 
 const Home: NextPage = () => {
+  React.useEffect(() => {
+    (async () => {
+      await waitReady();
+      // generate phrase
+      const phrase = bip39Generate(12);
+
+      // get ed25519 seed from phrase
+      const seed = bip39ToSeed(phrase, "");
+
+      // display
+      console.log("phrase:", phrase);
+      console.log("seed:", u8aToHex(seed));
+    })();
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -18,7 +36,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
@@ -59,14 +77,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
